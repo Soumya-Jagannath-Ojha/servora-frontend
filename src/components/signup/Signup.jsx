4e9ffd78644afb77e7979a583dcd814a1b0846f8
Soumya-Jagannath-Ojha <soumyajagannathojha7@gmail.com>
2026-01-01 11:23:32 +0530

    navigate added

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Signup = () => {
 
@@ -9,6 +9,7 @@ const Signup = () => {
     email: "",
     password: ""
   });
+  const navigate = useNavigate();
 
   const handlchange = (e) => {
     const { name, value } = e.target;
